37334c85e52b7350202d00b15f9922fa12a393bc
fernandohs <hernandezs1h@hotmail.com>
2020-09-12 20:28:10 -0500

    VersionEstable

--- Estacionamiento/administrador.cpp
+++ Estacionamiento/administrador.cpp
@@ -11,9 +11,9 @@ Administrador::Administrador(QWidget *parent) :
     titulos << "Folio" << "Descripcion" << "Fecha" ;
     ui->tablacomentarios->setHorizontalHeaderLabels(titulos);
 
-    db = QSqlDatabase::addDatabase("QODBC");
+    /*db = QSqlDatabase::addDatabase("QODBC");
     db.setUserName("root");
-    db.setDatabaseName("ParkingALot");
+    db.setDatabaseName("ParkingALot");*/
 
     if(db.open()){
         QSqlQuery comentarios;
@@ -25,65 +25,59 @@ Administrador::Administrador(QWidget *parent) :
                 QString des = comentarios.value(1).toString();
                 QString fec = comentarios.value(2).toString();
 
-            ui->tablacomentarios->insertRow(ui->tablacomentarios->rowCount());
-            ui->tablacomentarios->setItem(ui->tablacomentarios->rowCount()-1,0,new QTableWidgetItem(fol));
-             ui->tablacomentarios->setItem(ui->tablacomentarios->rowCount()-1,1,new QTableWidgetItem(des));
-              ui->tablacomentarios->setItem(ui->tablacomentarios->rowCount()-1,2,new QTableWidgetItem(fec));
+                ui->tablacomentarios->insertRow(ui->tablacomentarios->rowCount());
+                ui->tablacomentarios->setItem(ui->tablacomentarios->rowCount()-1,0,new QTableWidgetItem(fol));
+                ui->tablacomentarios->setItem(ui->tablacomentarios->rowCount()-1,1,new QTableWidgetItem(des));
+                ui->tablacomentarios->setItem(ui->tablacomentarios->rowCount()-1,2,new QTableWidgetItem(fec));
 
             }
         }
-    }
 
-     QSqlQuery Tarifas;
-     if(Tarifas.exec("Select Descripcion , Monto from Tarifa where idTarifa = 1 ;")){
+        QSqlQuery Tarifas;
+        if(Tarifas.exec("Select Descripcion , Monto from Tarifa where idTarifa = 1 ;")){
             while(Tarifas.next()){
-         QString Descripcion=Tarifas.value(0).toString();
-          QString monto =Tarifas.value(1).toString();
+                QString Descripcion=Tarifas.value(0).toString();
+                QString monto =Tarifas.value(1).toString();
+                ui->TarifaActual1->setText(monto);
+                ui->TipoTarifa1->setText(Descripcion);
+            }
+        }
 
-         ui->TarifaActual1->setText(monto);
-         ui->TipoTarifa1->setText(Descripcion);
-     }
 
-            QSqlQuery Tarifas2;
-            if(Tarifas2.exec("Select Descripcion , Monto from Tarifa where idTarifa = 2 ;")){
-                   while(Tarifas2.next()){
+
+        QSqlQuery Tarifas2;
+        if(Tarifas2.exec("Select Descripcion , Monto from Tarifa where idTarifa = 2 ;")){
+               while(Tarifas2.next()){
                 QString Descripcion2=Tarifas2.value(0).toString();
-                 QString monto2 =Tarifas2.value(1).toString();
-
+                QString monto2 =Tarifas2.value(1).toString();
                 ui->TarifaActual2->setText(monto2);
                 ui->TipoTarifa2->setText(Descripcion2);
             }
-     }
+        }
 
+        QStringList titulos2;
+        ui->Registros->setColumnCount(5);
+        titulos2 << "Nombre" << "Id Cliente" << "Fecha" << "Hora de entrada" << "Hora de salida";
+        ui->Registros->setHorizontalHeaderLabels(titulos2);
+        QSqlQuery Registros;
+        if(Registros.exec("SELECT U.Nombre, U.ApellidoP, U.ApellidoM, R.idUsuario, R.fecha, R.horaEntrada, R.horaSalida FROM usuario as U INNER JOIN cliente as C ON U.idUsuario=C.idUsuario INNER JOIN reservacionunica as R ON C.idUsuario=R.idUsuario;")){
+           while(Registros.next()){
+               QString nombrecompleto = Registros.value(0).toString()+Registros.value(1).toString()+Registros.value(2).toString();
+               QString idc = Registros.value(3).toString();
+               QString fec = Registros.value(4).toString();
+               QString he = Registros.value(5).toString();
+               QString hs = Registros.value(6).toString();
 
-}
-     ui->setupUi(this);
-     QStringList titulos2;
-     ui->Registros->setColumnCount(5);
-     titulos2 << "Nombre" << "Id Cliente" << "Fecha" << "Hora de entrada" << "Hora de salida";
-     ui->Registros->setHorizontalHeaderLabels(titulos2);
-
-     if(db.open()){
-         QSqlQuery Registros;
-         if(Registros.exec("SELECT U.Nombre, U.ApellidoP, U.ApellidoM, R.idUsuario, R.fecha, R.horaEntrada, R.horaSalida FROM usuario as U INNER JOIN cliente as C ON U.idUsuario=C.idUsuario INNER JOIN reservacionunica as R ON C.idUsuario=R.idUsuario;")){
-             while(Registros.next()){
-                 QString nombrecompleto = Registros.value(0).toString()+Registros.value(1).toString()+Registros.value(2).toString();
-                 QString idc = Registros.value(3).toString();
-                 QString fec = Registros.value(4).toString();
-                 QString he = Registros.value(5).toString();
-                 QString hs = Registros.value(6).toString();
-
-             ui->Registros->insertRow(ui->Registros->rowCount());
-             ui->Registros->setItem(ui->Registros->rowCount()-1,0,new QTableWidgetItem(nombrecompleto));
-              ui->Registros->setItem(ui->Registros->rowCount()-1,1,new QTableWidgetItem(idc));
+               ui->Registros->insertRow(ui->Registros->rowCount());
+               ui->Registros->setItem(ui->Registros->rowCount()-1,0,new QTableWidgetItem(nombrecompleto));
+               ui->Registros->setItem(ui->Registros->rowCount()-1,1,new QTableWidgetItem(idc));
                ui->Registros->setItem(ui->Registros->rowCount()-1,2,new QTableWidgetItem(fec));
                ui->Registros->setItem(ui->Registros->rowCount()-1,3,new QTableWidgetItem(he));
                ui->Registros->setItem(ui->Registros->rowCount()-1,4,new QTableWidgetItem(hs));
-
-             }
-         }
-     }
-
+            }
+        }
+        db.close();
+    }
 }
 
 Administrador::~Administrador()
@@ -121,6 +115,7 @@ void Administrador::on_Acualizar1_clicked()
    actu.prepare("CALL CAMBIARTARIFAESTANDAR(1007,:NU);");
    actu.bindValue(":NU",Nuevo);
    actu.exec();
+   db.close();
 
 }
 
@@ -132,10 +127,11 @@ void Administrador::on_actualizar2_clicked()
     actual.prepare("CALL CAMBIATARIFAMENSUAL(1007,:NU);");
     actual.bindValue(":NU",Nuevo);
     actual.exec();
+    db.close();
 }
 
-void Administrador::on_pushButton_clicked()
+/*void Administrador::on_pushButton_clicked()
 {
 
-}
+}*/
 
